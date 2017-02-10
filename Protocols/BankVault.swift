82871c7925c216fdae36fca47902782b865a3664
Marc Aupont <marc@digimarktech.com>
2017-02-09 22:51:48 -0500

    Completed Protocol Lab with tests passing

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+protocol ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+    
+}
 
 class BankVault {
     
@@ -18,6 +23,54 @@ class BankVault {
         self.address = address
     }
     
+}
+
+extension BankVault: ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        
+        var amountOfEvens = 0
+        
+        if password.isEmpty {
+            
+            return false
+        }
+        
+        if password.count > 10 {
+            
+            return false
+        }
+        
+        for i in 0 ..< password.count {
+            
+            if i % 2 == 0 {
+                
+                if password[i] % 2 == 0 {
+                    
+                    amountOfEvens += 1
+                }
+            }
+        }
+        
+        if password.count % 2 != 0 {
+            
+            if amountOfEvens == (password.count + 1) / 2 {
+                
+                return true
+            }
+            
+        } else {
+            
+            if amountOfEvens == password.count  / 2 {
+                
+                return true
+            }
+        }
+        
+        
+        
+        return false
+    }
 }
 
 
